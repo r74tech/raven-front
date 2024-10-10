b614d8c785c166f62d2415f384ba4832f39c3fd2
r74tech <r74tech@gmail.com>
2024-10-11 01:33:31 +0900

    fix gh pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.GITHUB_PAGES
-    ? 'raven-front' // レポジトリ名を設定
-    : './'
+  base: './'
 })
